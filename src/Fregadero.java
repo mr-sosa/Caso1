090b04cdddb8d1de4c2107a5178cee330eb3a9f2
Manuel Sosa <mr.sosa@uniandes.edu.co>
2021-09-07 23:35:35 -0500

    ajustes finales

--- src/Fregadero.java
+++ src/Fregadero.java
@@ -2,20 +2,26 @@
 public class Fregadero {
 	private int tamFregadero;
 	
-	private int n;
+	private int nT1;
+	private int nT2;
 	
 	public Fregadero(int tamFregadero2) {
 		tamFregadero = tamFregadero2;
-		n = 0;
+		nT1 = 0;
+		nT2 = 0;
 	}
 	
-	public synchronized void agregarCubierto() {
+	public synchronized void agregarCubierto(int t) {
 		try {
-			while(n==tamFregadero) {
+			while((nT1 + nT2) ==tamFregadero) {
 				wait();
 			}
-			n++;
-			System.out.println("Se agregó un cubierto al Fregadero, stock actual: " + n);
+			if(t == 1) {
+				nT1++;
+			} else if (t == 2) {
+				nT2++;
+			}
+			System.out.println("Se agregó un cubierto al Fregadero, stock actual T1: " + nT1+ " T2:"+ nT2);
 			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -25,10 +31,15 @@ public class Fregadero {
 	public synchronized int quitarCubierto() {
 		int resp = 0;
 		try {
-			if(n!=0) {
-				n--;
+			if(nT1!=0) {
+				nT1--;
 				resp = 1;
-				System.out.println("Se quitó un cubierto al Fregadero, stock actual: " + n);
+				System.out.println("Se quitó un cubierto al Fregadero, stock actual: T1: " + nT1+ " T2:"+ nT2);
+			}
+			else if(nT1!=0) {
+				nT2--;
+				resp = 2;
+				System.out.println("Se quitó un cubierto al Fregadero, stock actual: T1: " + nT1+ " T2:"+ nT2);
 			}
 			
 			
